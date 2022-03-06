7bd100aadeeb1b4cff90fcb4f6f1a6277209e6e5
Rodolfo <rqm@outlook.es>
2022-03-05 18:22:14 -0600

    Fin

--- sitema/src/App.jsx
+++ sitema/src/App.jsx
@@ -1,49 +1,67 @@
-import axios from "axios"
-import { useState, useEffect } from "react"
-import "./App.css"
-import "bootstrap/dist/css/bootstrap.min.css"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import './App.css';
+import { useEffect, useState } from 'react';
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
 
-  const [usuario, setUsuario] = useState([]);
-  const [tablaUsuario, setTablaUsuario] = useState([]);
+  const [usuarios, setUsuarios] = useState([]);
+  const [tablaUsuarios, setTablaUsuarios] = useState([]);
   const [busqueda, setBusqueda] = useState("");
 
-  const peticionget = async () => {
-    axios.get("https://jsonplaceholder.typicode.com/users")
-      .then(res => {
-        setUsuario(res.data);
-        setTablaUsuario(res.data);
-      }).catch(err => {
-        console.error(err);
+  const peticionGet = async () => {
+    await axios.get("https://jsonplaceholder.typicode.com/users")
+      .then(response => {
+        setUsuarios(response.data);
+        setTablaUsuarios(response.data);
+      }).catch(error => {
+        console.log(error);
       })
-  };
-  useEffect(() => {
-    peticionget()
+  }
 
-  }, []);
+  const handleChange = e => {
+    setBusqueda(e.target.value);
+    filtrar(e.target.value);
+  }
+
+  const filtrar = (terminoBusqueda) => {
+    var resultadosBusqueda = tablaUsuarios.filter((elemento) => {
+      if (elemento.name.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
+        || elemento.company.name.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
+      ) {
+        return elemento;
+      }
+    });
+    setUsuarios(resultadosBusqueda);
+  }
+
+  useEffect(() => {
+    peticionGet();
+  }, [])
 
   return (
-    <>
+    <div className="App">
       <div className="containerInput">
         <input
           className="form-control inputBuscar"
           value={busqueda}
-          placeholder="Busqueda por nombre"
+          placeholder="Búsqueda por Nombre o Empresa"
+          onChange={handleChange}
         />
         <button className="btn btn-success">
           <FontAwesomeIcon icon={faSearch} />
         </button>
       </div>
-      <div className="table-reponsive">
-        <div className="table table-sm table-bordered">
+
+      <div className="table-responsive">
+        <table className="table table-sm table-bordered">
           <thead>
             <tr>
               <th>ID</th>
               <th>Nombre</th>
-              <th>Telefono</th>
+              <th>Teléfono</th>
               <th>Nombre de Usuario</th>
               <th>Correo</th>
               <th>Sitio Web</th>
@@ -53,25 +71,26 @@ function App() {
           </thead>
 
           <tbody>
-            {usuario &&
-              usuario.map((usua) => (
-
-                <tr key={usua.id} >
-                  <td>{usua.id}</td>
-                  <td>{usua.name}</td>
-                  <td>{usua.phone}</td>
-                  <td>{usua.username}</td>
-                  <td>{usua.email}</td>
-                  <td>{usua.website}</td>
-                  <td>{usua.address.city}</td>
-                  <td>{usua.company.name}</td>
+            {usuarios &&
+              usuarios.map((usuario) => (
+                <tr key={usuario.id}>
+                  <td>{usuario.id}</td>
+                  <td>{usuario.name}</td>
+                  <td>{usuario.phone}</td>
+                  <td>{usuario.username}</td>
+                  <td>{usuario.email}</td>
+                  <td>{usuario.website}</td>
+                  <td>{usuario.address.city}</td>
+                  <td>{usuario.company.name}</td>
                 </tr>
               ))}
           </tbody>
-        </div>
+
+        </table>
+
       </div>
-    </ >
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
